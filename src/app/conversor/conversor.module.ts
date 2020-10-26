0fc35a0383be7ef3d700b27cab703e3b9db5f0de
diegominelli <diego.minelli@hotmail.com>
2020-10-26 16:59:26 -0300

    Criando as rotas do módulo Conversor de moedas

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
 import { NumeroDirective } from './directives';
 import { ModalCotacaoComponent } from './utils';
 import { DataBrPipe } from './pipes';
+import { ConversorRoutingComponent } from './conversor-routing.component';
+import { ConversorRoutingModule } from './conversor-routing.module';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    ConversorRoutingModule
   ],
   declarations: [
     ConversorComponent,
     NumeroDirective,
     ModalCotacaoComponent,
-    DataBrPipe
+    DataBrPipe,
+    ConversorRoutingComponent
   ],
   exports: [
     ConversorComponent
